dc89aa2b6c15dc8d06907343a08bbe067f7d75f5
ArgaAAL <argalumunon9@gmail.com>
2024-04-25 22:49:53 +0700

    add unique email regis

--- src/Model/UserModel.java
+++ src/Model/UserModel.java
@@ -11,20 +11,37 @@ public class UserModel {
     }
 
     public void createDonaturAccount(String idDonatur, String email, String username, String password) {
-        users.add(new Donatur(idDonatur, email, username, password));
-        System.out.println("Donatur account created with ID: " + idDonatur);
+        if (isEmailUnique(email)) {
+            users.add(new Donatur(idDonatur, email, username, password));
+            System.out.println("Donatur account created with ID: " + idDonatur);
+        } else {
+            System.out.println("Email is already registered.");
+        }
     }
 
     public void createAdminAccount(String idAdmin, String email, String username, String password) {
         users.add(new Admin(idAdmin, email, username, password));
         System.out.println("Admin account created with ID: " + idAdmin);
+        //bingung gimn wkwk
     }
 
     public void createOrganisasiAccount(String idOrganisasi, String email, String username, String password) {
-        users.add(new Organisasi(idOrganisasi, email, username, password));
-        System.out.println("Organisasi account created with ID: " + idOrganisasi);
+        if (isEmailUnique(email)) {
+            users.add(new Organisasi(idOrganisasi, email, username, password));
+            System.out.println("Organisasi account created with ID: " + idOrganisasi);
+        } else {
+            System.out.println("Email is already registered.");
+        }
     }
 
+    private boolean isEmailUnique(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return false;
+            }
+        }
+        return true;
+    }
     public List<User> getUsers() {
         return users;
     }
