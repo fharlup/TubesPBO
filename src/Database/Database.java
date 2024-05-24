ce5be1b33eb930ea5ec4be3fbcc60272c40ff2c7
alvanmarvianto <153970041+alvanmarvianto@users.noreply.github.com>
2024-05-24 15:43:12 +0700

    Update Database.java

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Database;
 
 import java.sql.Statement;
@@ -13,42 +8,20 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Alvan
- */
 public class Database {
-    private String DB_URL="jdbc:mysql://127.0.0.1/pbo";
-    private String DB_USER ="root";
-    private String DB_PASS="";
-    static Connection conn;
-    static Statement stmt;
-    static ResultSet rs;
+    private static final String DB_URL="jdbc:mysql://127.0.0.1/pbo";
+    private static final String DB_USER ="root";
+    private static final String DB_PASS="";
+    private static Connection conn;
     
-    public Database(){
+    public static Connection getConnection(){
         try {
             conn = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS);
-            stmt = conn.createStatement();
+            return conn;
         } catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return null;
         
-    }
-    
-    public ResultSet getData(String SQLString){
-        try {
-            rs = stmt.executeQuery(SQLString);
-        } catch (SQLException ex) {
-            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return rs;
-    }
-    
-    public void query (String SQLString){
-        try {
-            stmt.executeUpdate(SQLString);
-        } catch (SQLException ex) {
-            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 }
